12912caf123de0d34a0a7764341141868950e082
MrWhiteRichard <richard.weiss.tuwien@gmail.com>
2020-01-17 20:58:35 +0100

    Numerik Projekt 2: Aufgabe2: Weitergemacht, bis Aufgabe e) (unfertig) gekommen;

---
+++ WS_2019-20/Numerik_UE/Projekt_2/Aufgabe_2/python_code/get_zero_function.py
@@ -0,0 +1,27 @@
+# c ... pair of propagation speeds
+
+def get_zero_function(c):
+
+    # allocate some sympy symbols:
+    omega = sp.Symbol('\omega')
+    kappa = sp.IndexedBase('\kappa')
+
+    # implement the matrix R (properly):
+    R = sp.Matrix([[ sp.sin(kappa[0]/2),  kappa[0]*sp.cos(kappa[0]/2), 0],
+                   [-sp.cos(kappa[1]/2),  kappa[1]*sp.sin(kappa[1]/2), sp.cos(kappa[1])],
+                   [-sp.sin(kappa[1]/2), -kappa[1]*sp.cos(kappa[1]/2), sp.sin(kappa[1])]])
+    R = R.T
+
+    # calculate R's determinant (properly):
+    det = sp.det(R)
+    det = sp.simplify(det)
+
+    # substitute for kappa_0 and kappa_1:
+    kappa_0 = omega/c[0]
+    kappa_1 = omega/c[1]
+    det = det.subs({kappa[0]: kappa_0, kappa[1]: kappa_1})
+
+    # transform expression det into proper numpy function:
+    zero_function = sp.lambdify(omega, det, 'numpy')
+
+    return zero_function
