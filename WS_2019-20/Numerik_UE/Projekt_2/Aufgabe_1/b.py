1c6221935b1096bba6ce27235c6eab5060805747
Declaminius <schagerflorian@gmail.com>
2020-01-29 19:56:38 +0100

    Aufgabe 1

--- WS_2019-20/Numerik_UE/Projekt_2/Aufgabe_1/b.py
+++ WS_2019-20/Numerik_UE/Projekt_2/Aufgabe_1/b.py
@@ -21,23 +21,33 @@ def cg(A,b,x0,tol):
         count += 1
     return xt, np.array(res), count
 
+def zufallsmatrix(n, nonzeros):
+	A = np.concatenate((np.zeros((n,nonzeros)), np.random.rand(n, n-nonzeros)), axis = 1)
+	for i in range(n):
+		np.random.shuffle(A[i])
+	return A + A.T + np.diag(np.random.rand(n)*n)
+
 n = 5000
-A = np.random.rand(n,n)
-A = np.dot(A,np.transpose(A))
-A += 10*np.diag(abs(np.random.random(n)))
+A = zufallsmatrix(n,20)
 b = np.random.rand(n)
 tol = 10**(-8)
-x0 = np.random.rand(n)*10
-
+x0 = np.random.rand(n)
+cond = np.linalg.cond(A)
+c = (1-1/np.sqrt(cond))/(1+1/np.sqrt(cond))
+print(cond)
+print(c)
 xsolve = np.linalg.solve(A,b)
 
 xcg, res, count = cg(A,b,x0,tol)
 x = [i+1 for i in range(len(res))]
 q = b-A@xcg
 print(np.linalg.norm(q))
-y = [0.92**i for i in x]
+y1 = [c**i for i in x]
+#y2 = [0.9**i for i in x]
 plt.semilogy(x, res, label = "Residuum")
-plt.semilogy(x, y, linestyle = "dotted", label = "0.92^t")
+plt.semilogy(x, y1, linestyle = "dotted", label = "B^t")
+#plt.semilogy(x, y2, linestyle = "dotted", label = "0.9^t")
 plt.xlabel("Iterationen")
+plt.title("n = {0}, cond(A) = {1}, B = {2}".format(n, cond, c))
 plt.legend()
 plt.show()
--- WS_2019-20/Numerik_UE/Projekt_2/Aufgabe_1/e.py
+++ WS_2019-20/Numerik_UE/Projekt_2/Aufgabe_1/e.py
@@ -1,30 +1,34 @@
 import numpy as np
 import time
 from c import *
-import matplotlib.pyplot as plt
 
-n = 2000
-k = 200
+
+n = 1000
 z = 10
 b = np.random.rand(n)
 x0 = np.random.rand(n)
 tol = 10**-10
 A = np.zeros((n,n))
 start = time.process_time()
+row_sum = []
 for i in range(n):
     non_zeros = np.random.rand(z)
     zeros = np.zeros(n-z)
     A[i] = np.concatenate((non_zeros, zeros))
     np.random.shuffle(A[i])
-A = A + A.T + np.diag(np.random.rand(n)*100)
+    #row_sum.append(abs(np.sum(A[i]))+1)
+A = A + A.T + np.diag(np.random.rand(n)*10)
 end = time.process_time()
 D = np.diag(np.diagonal(np.sqrt(A)))
-#print("Kondition alt: {}".format(np.linalg.cond(A)))
-#print("Kondition neu: {}".format(np.linalg.cond((np.linalg.inv(D)@A)@(np.linalg.inv(D).T))))
+print("Kondition alt: {}".format(np.linalg.cond(A)))
+print("Kondition neu: {}".format(np.linalg.cond((np.linalg.inv(D)@A)@(np.linalg.inv(D).T))))
+
+A = Sparse(False, A)
 
 print("Zeit Random:{}".format(end-start))
 
 P = D@D.T
+<<<<<<< HEAD
 x = [i for i in range(k,n+1,k)]
 y1 = []
 y2 = []
@@ -42,5 +46,14 @@ plt.plot(x,y1, label= "Standard", linestyle = "dotted")
 plt.plot(x,y2, label = "Vorkonditioniert")
 plt.xlabel("Matrixdimension")
 plt.ylabel("Anzahl Iterationen")
+plt.title("beliebig s.p.d.")
 plt.legend()
 plt.show()
+=======
+
+start = time.process_time()
+xt = vcg(A,b,x0,P,tol)
+end = time.process_time()
+print("Zeit: {}".format(end - start))
+print(np.linalg.norm(A.__matmult__(xt)-b))
+>>>>>>> 551652d2362fcc5cd8ee0c76fc77369d0479ff4e
