f8a76db93a91cd42aefad8c0ec2d1d422e2b55ae
Jakob Guttmann <jakob.guttmann98@gmail.com>
2020-01-29 19:49:24 +0100

    Numerik Aufgabe, neuer Plot

--- WS_2019-20/Numerik_UE/Projekt_2/Aufgabe_1/e.py
+++ WS_2019-20/Numerik_UE/Projekt_2/Aufgabe_1/e.py
@@ -1,10 +1,9 @@
 import numpy as np
 import time
 from c import *
-import matplotlib.pyplot as plt
 
-n = 2000
-k = 200
+
+n = 1000
 z = 10
 b = np.random.rand(n)
 x0 = np.random.rand(n)
@@ -19,28 +18,17 @@ for i in range(n):
 A = A + A.T + np.diag(np.random.rand(n)*100)
 end = time.process_time()
 D = np.diag(np.diagonal(np.sqrt(A)))
-#print("Kondition alt: {}".format(np.linalg.cond(A)))
-#print("Kondition neu: {}".format(np.linalg.cond((np.linalg.inv(D)@A)@(np.linalg.inv(D).T))))
+print("Kondition alt: {}".format(np.linalg.cond(A)))
+print("Kondition neu: {}".format(np.linalg.cond((np.linalg.inv(D)@A)@(np.linalg.inv(D).T))))
+
+A = Sparse(False, A)
 
 print("Zeit Random:{}".format(end-start))
 
 P = D@D.T
-x = [i for i in range(k,n+1,k)]
-y1 = []
-y2 = []
-for i in range(k,n+1,k):
-    A_i = A[:i,:i]
-    b_i = b[:i]
-    x0_i = x0[:i]
-    P_i = P[:i,:i]
-    A_i = Sparse(False, A_i)
-    xt, c = Scg(A_i,b_i,x0_i,tol)
-    y1.append(c)
-    xt, c = vcg(A_i,b_i,x0_i,P_i,tol)
-    y2.append(c)
-plt.plot(x,y1, label= "Standard", linestyle = "dotted")
-plt.plot(x,y2, label = "Vorkonditioniert")
-plt.xlabel("Matrixdimension")
-plt.ylabel("Anzahl Iterationen")
-plt.legend()
-plt.show()
+
+start = time.process_time()
+xt = vcg(A,b,x0,P,tol)
+end = time.process_time()
+print("Zeit: {}".format(end - start))
+print(np.linalg.norm(A.__matmult__(xt)-b))
--- WS_2019-20/Numerik_UE/Projekt_2/Aufgabe_1/plots.py
+++ WS_2019-20/Numerik_UE/Projekt_2/Aufgabe_1/plots.py
@@ -10,14 +10,14 @@ def zufallsmatrix(z,n):
         zeros = np.zeros(n-z)
         A[i] = np.concatenate((non_zeros, zeros))
         np.random.shuffle(A[i])
-    A = A + A.T + np.diag(np.random.rand(n)*1000)
+    A = A + A.T + np.diag(np.random.rand(n)*3000)
     return A
 
 n = 10000
-z = 60
+z = 30
 A = zufallsmatrix(z,n)
 
-x = np.array([i for i in range(9000,n+1,200)])
+x = np.array([i for i in range(500,n+1,100)])
 time_np = []
 time_sparse = []
 
@@ -27,7 +27,7 @@ for i in x:
     A_part = A[:i,:i]
     b = np.random.rand(i)*100
     start = time.process_time()
-    c=np.dot(A_part,b)
+    c=A_part@b
     end =time.process_time()
     diff = end-start
     time_np.append(diff)
@@ -40,7 +40,6 @@ for i in x:
     time_sparse.append(diff)
     print(i)
 
-plt.loglog(x,(x/10000)**2,'--',label="O(n^2)")
 plt.loglog(x,time_np,'-',label="numpy")
 plt.loglog(x,time_sparse,':',label = "Sparse")
 plt.xlabel("Matrix-Dimension")
@@ -50,7 +49,7 @@ plt.show()
 
 # n = 3000
 # x = np.array([i for i in range(100,n+1,50)])
-# z = 60
+# z = 30
 # A = zufallsmatrix(z,n)
 # tol = 10**(-8)
 # time_cg = []
@@ -83,3 +82,41 @@ plt.show()
 # plt.ylabel("Sekunden")
 # plt.legend()
 # plt.show()
+
+# n = 10000
+# z = 30
+# A = zufallsmatrix(z,n)
+# tol = 10**(-8)
+#
+# x = np.array([i for i in range(500,n+1,500)])
+# time_np = []
+# time_cg = []
+#
+# for i in x:
+#     start = 0
+#     end = 0
+#     A_part = A[:i,:i]
+#     b = np.random.rand(i)*100
+#     x_0 = np.random.rand(i)*100
+#     start = time.process_time()
+#     c=np.linalg.solve(A_part,b)
+#     end =time.process_time()
+#     diff = end-start
+#     time_np.append(diff)
+#
+#     A_sparse = Sparse(False,A_part)
+#     start = time.process_time()
+#     c=Scg(A_sparse,b,x_0,tol)
+#     end = time.process_time()
+#     diff=end-start
+#     time_cg.append(diff)
+#     print(i)
+#
+# plt.loglog(x,time_cg,'-',label="cg-Verfahren (Sparse)")
+# plt.loglog(x,time_np,':',label = "np.linalg.solve")
+# plt.xlabel("Matrix-Dimension")
+# plt.ylabel("Sekunden")
+# plt.legend()
+# plt.show()
+
+
