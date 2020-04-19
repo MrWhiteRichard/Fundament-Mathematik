140aaa31d1b487f57b11cb19af4eed1e3cc6729b
DerKleon <zehetgruber.fabian31@gmail.com>
2020-04-19 17:12:15 +0200

    Ein Versuch Algebra 2.1.6.6. verständlicher hinzuschreiben

---
+++ SS_2020/UE_ODEs/Übung_5/5_6.py
@@ -0,0 +1,10 @@
+import sympy as sp
+sp.init_printing(use_unicode=True)
+
+t = sp.symbols('t')
+
+A1 = sp.Matrix([[-1, 1, -1], [2, -1, 2], [2, 2, -1]])
+V1, D1 = A1.diagonalize()
+print(A1 ,"=", V1 , D1,  V1**-1)
+F1 = sp.exp(t * D1)
+print(V1 * F1 * V1**-1)
