a3a18a7d3b97d416d1b6fb10058c68836883bf2f
DerKleon <zehetgruber.fabian31@gmail.com>
2020-05-06 19:59:52 +0200

    ode uebung7 aufgabe3 derzeitiger stand in python

--- SS_2020/UE_ODEs/Übung_7/7-3.py
+++ SS_2020/UE_ODEs/Übung_7/7-3.py
@@ -1,12 +1,18 @@
 import sympy as sp
 
-epsilon, t = sp.symbols('epsilon, t')
-y = sp.symbols('y', cls=sp.Function)
+epsilon, t, a1, a2 = sp.symbols('epsilon, t, a_1, a_2')
+y1 = sp.symbols('y_1', cls=sp.Function)
 
-diffeq = sp.Eq(y(t).diff(t, 2) + y(t) + epsilon * y(t)**3, 0)
+diffeq = sp.Eq(y1(t).diff(t, 2) + y1(t), -(sp.cos(t))**3)
 
 sp.pretty_print(diffeq)
 
-sol = sp.dsolve(diffeq, y(t), ics={y(0): 1, y(t).diff(t).subs(t, 0): 0})
+sol = sp.dsolve(diffeq, y1(t))
 
 sp.pretty_print(sol)
+
+# y1 = 1/(2j - 2*t) * sp.exp(t*1j) + 1/(-2j -2*t) * sp.exp(-t*1j) + sp.Rational(1, 5) * sp.cos(3*t)
+
+# sp.pretty_print(y1)
+
+# sp.pretty_print(sp.simplify(y1.diff(t, 2) + y1))
