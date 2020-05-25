75d766555c98c891100e0a47ca95dca551fa822a
Declaminius <schagerflorian@gmail.com>
2020-05-25 16:57:27 +0200

    NumODEs 41 Code vollständig

--- SS_2020/UE_NumODEs/Übung_9/9.1.py
+++ SS_2020/UE_NumODEs/Übung_9/9.1.py
@@ -4,11 +4,24 @@ t = sp.Symbol("t")
 
 
 L = lambda t : np.prod([(t - (1 - i))/(i - j) for i in range(k+1) if i != j])
-coeff = []
+first_roots = []
+simplicity_check = []
+coeffs = []
 for k in range(1,11):
-    coeff.append([])
+    coeffs.append([])
     for j in range(k+1):
         D = sp.lambdify(t,sp.diff(L(t),t),"numpy")
-        coeff[-1].append(D(1))
+        coeffs[-1].append(D(1))
+    n = len(coeffs[-1]) - 1
+    first_roots.append(np.roots(coeffs[-1]))
 
-print(coeff)
+
+for i in range(len(first_roots)):
+    print("k = {}".format(i+1))
+    print([abs(first_roots[i][j]) for j in range(len(first_roots[i]))])
+    for j in range(len(first_roots[i])):
+        if i < 6:
+            if abs(first_roots[i][j]) > 1 - 1e-6:
+                n = len(coeffs[i]) - 1
+                p = [n*coeffs[i][k] for k in range(n)]
+                print(np.polyval(p, first_roots[i][j]))
