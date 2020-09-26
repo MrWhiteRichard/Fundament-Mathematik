144290d1aaa6dd121e97c18fa2bcc7076da19bda
Richard Weiss <richard.weiss.tuwien@gmail.com>
2020-09-26 14:31:36 +0200

    Setup-LaTeX-Project: Update & Test with PDEs UE1

--- Fundament-Tools/Setup-LaTeX-Project/functions.py
+++ Fundament-Tools/Setup-LaTeX-Project/functions.py
@@ -3,6 +3,7 @@
 import os
 import shutil
 import itertools
+import codecs
 
 # ---------------------------------------------------------------- #
 
@@ -25,54 +26,68 @@ path_source_folder = '\\'.join(path_script_folder_dissected[:-2:] + ['Fundament-
 
 # ---------------------------------------------------------------- #
 
-def setup_exercises_and_solutions(path_project_folder, Numbers):
+def exercise_and_solution_file_names(path_exercise_folder, lva_name, exercise_number, exercise_amount, exercise_date):
 
-    destination_file_names = ['.'.join([str(number) for number in numbers]) + '.tex' for numbers in itertools.product(*Numbers)]
-
-    for destination_file_name in destination_file_names:
-
-        # copy 'exercise and solution.tex' as destination_file_name
-        path_source_file      = path_source_folder  + '\\' + 'exercise and solution.tex'
-        path_destination_file = path_project_folder + '\\' + destination_file_name
-        shutil.copyfile(path_source_file, path_destination_file)
-
-    return destination_file_names
+    Numbers = itertools.product(*[[exercise_number], range(1, exercise_amount+1)])
+    destination_file_name  = lambda numbers: '.'.join([str(number) for number in numbers]) + '.tex'
+    return [destination_file_name(numbers) for numbers in Numbers]
 
 # ---------------------------------------------------------------- #
 
-def setup_main(path_project_folder, subfile_names):
+def setup_exercises_and_solutions(path_exercise_folder, lva_name, exercise_number, exercise_amount, exercise_date):
+
+    for destination_file_name in exercise_and_solution_file_names(path_exercise_folder, lva_name, exercise_number, exercise_amount, exercise_date):
+
+        # copy 'exercise and solution.tex' as destination_file_name
+        path_source_file      = path_source_folder  + '\\' + 'exercise and solution.tex'
+        path_destination_file = path_exercise_folder + '\\' + destination_file_name
+        shutil.copyfile(path_source_file, path_destination_file)
+
+# ---------------------------------------------------------------- #
+
+def setup_exercise_main(path_exercise_folder, lva_name, exercise_number, exercise_amount, exercise_date):
     
-    path_source_file = path_project_folder + '\\' + 'main.tex'
+    # copy 'main.tex' as 'main.tex'
+    path_source_file      = path_source_folder   + '\\' + 'main.tex'
+    path_destination_file = path_exercise_folder + '\\' + 'main.tex'
+    shutil.copyfile(path_source_file, path_destination_file)
 
     # read content of 'main.tex' as array of lines
-    with open(path_source_file, 'r') as main:
+    with codecs.open(path_source_file, 'r', 'utf-8') as main:
         main_content_old = main.readlines()
 
-    # get index of line that says '\maketitle'
-    index = main_content_old.index(r'\maketitle'+ '\n')
-
     # stuff that gets added to 'main.tex'
-    main_content_add = [r'\input{' + subfile_name + '}\n' for subfile_name in subfile_names]
+    main_content_add = [r'\input{' + exercise_and_solution_file_name + '}\n' for exercise_and_solution_file_name in exercise_and_solution_file_names(path_exercise_folder, lva_name, exercise_number, exercise_amount, exercise_date)]
+
+    # get index of line that says '\maketitle'
+    index = main_content_old.index('\\maketitle\r\n')
 
     # new content of 'main.tex'
-    main_content_new = main_content_old[:index+1:] + ['\n'] + main_content_add+ main_content_old[index+1::]
+    main_content_new = main_content_old[:index+1:] + ['\n'] + main_content_add + main_content_old[index+1::]
+
+    x_1 = '  Titel \\\\\r\n'
+    y_1 = '  ' + lva_name + ' - ' + u'Übung' + ' ' + r'\\' + '\n'
+
+    x_2 = '  \\textit{Untertitel}\r\n'
+    y_2 = '  ' + r'\textit{' + f'{exercise_number}. UE am {exercise_date}' + r'}' + '\n'
+
+    for x, y in [(x_1, y_1), (x_2, y_2)]:
+
+        # get index of line that says x
+        index = main_content_new.index(x)
+
+        # change it to y
+        main_content_new[index] = y
 
     # write new content to 'main.tex'
-    with open(path_source_file, 'w') as main:
+    with codecs.open(path_destination_file, 'w', 'utf-8') as main:
         main.writelines(main_content_new)
 
 # ---------------------------------------------------------------- #
 
-def setup_exercise(path_project_folder, exercise_number, exercise_amount):
+def setup_exercise(path_exercise_folder, lva_name, exercise_number, exercise_amount, exercise_date):
 
-    # copy 'main.tex' as 'main.tex'
-    path_source_file      = path_source_folder  + '\\' + 'main.tex'
-    path_destination_file = path_project_folder + '\\' + 'main.tex'
-    shutil.copyfile(path_source_file, path_destination_file)
-
-    Numbers = [[exercise_number], range(1, exercise_amount+1)]
-    exercises_and_solution_file_names = setup_exercises_and_solutions(path_project_folder, Numbers)
-
-    setup_main(path_project_folder, exercises_and_solution_file_names)
+    setup_exercises_and_solutions(path_exercise_folder, lva_name, exercise_number, exercise_amount, exercise_date)
+    setup_exercise_main          (path_exercise_folder, lva_name, exercise_number, exercise_amount, exercise_date)
 
 # ---------------------------------------------------------------- #
--- Fundament-Tools/Setup-LaTeX-Project/setup_exercise.py
+++ Fundament-Tools/Setup-LaTeX-Project/setup_exercise.py
@@ -1,7 +1,9 @@
 from functions import setup_exercise
 
-path_project_folder = str(input('path_project_folder ... '))
-exercise_number     = int(input('exercise_number     ... '))
-exercise_amount     = int(input('exercise_amount     ... '))
+path_exercise_folder = str(input('path_exercise_folder ... '))
+lva_name             = str(input('lva_name             ... '))
+exercise_number      = int(input('exercise_number      ... '))
+exercise_amount      = int(input('exercise_amount      ... '))
+exercise_date        = str(input('exercise date        ... '))
 
-setup_exercise(path_project_folder, exercise_number, exercise_amount)
+setup_exercise(path_exercise_folder, lva_name, exercise_number, exercise_amount, exercise_date)
