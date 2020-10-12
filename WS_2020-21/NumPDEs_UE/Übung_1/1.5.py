766e7de5dc94ca97783f6f628d621331a9cfbfa6
Richard Weiss <richard.weiss.tuwien@gmail.com>
2020-10-12 23:22:12 +0200

    NumPDEs_UE: Übung_1: - TRAFO-Time!!!!!!!!! - included images

--- WS_2020-21/NumPDEs_UE/Übung_1/1.5.py
+++ WS_2020-21/NumPDEs_UE/Übung_1/1.5.py
@@ -2,35 +2,43 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import quad
 from fractions import Fraction
+
+# from scipy.integrate import quad
 
 # ---------------------------------------------------------------- #
 
-beta = lambda n: n / np.sqrt(4 * n**2 - 1)
-b = lambda n: np.array([beta(i) for i in range(1, n)])
-T = lambda n: np.diag(b(n), 1) + np.diag(b(n), -1)
+beta_scalar = lambda n: n / np.sqrt(4 * n**2 - 1)
+beta_vector = lambda n: np.array([beta_scalar(i) for i in range(1, n)])
+T = lambda n: np.diag(beta_vector(n), 1) + np.diag(beta_vector(n), -1)
 L = lambda nodes, i, x: np.prod(
     [(x - nodes[j]) / (nodes[i] - nodes[j]) for j in range(len(nodes)) if nodes[i] != nodes[j]]
 )
 
-def weights_nodes(a, b, n):
+def weights_nodes(n):
 
-    nodes, vectors = np.linalg.eig(T(n))
-    weights = [quad(lambda x: L(nodes, i, x), a, b)[0] for i in range(n)]
-    vectors = np.transpose(vectors)
-    weights2 = [2*(v[0]/np.linalg.norm(v))**2 for v in vectors]
-    return np.array(list(zip(weights, nodes)))
+    eigen_values, eigen_vectors = np.linalg.eig(T(n))
+    eigen_vectors = np.transpose(eigen_vectors)
 
-gaussian_quadrature = lambda a, b, n, f: sum([weight * f(node) for weight, node in weights_nodes(a, b, n)])
-print(weights_nodes(-1,1,3))
+    nodes = eigen_values
+
+    # weights = [quad(lambda x: L(nodes, i, x), a, b)[0] for i in range(n)]
+    weights = [(v[0] / np.linalg.norm(v))**2 * 2 for v in eigen_vectors]
+
+    return zip(weights, nodes)
+
+quad_gauss_reference = lambda n, f: sum([weight * f(node) for weight, node in weights_nodes(n)])
+
+Phi = lambda a, b, xi: (2 * xi - (a + b)) / (b - a)
+quad_gauss = lambda a, b, n, f: 2 / (b - a) * quad_gauss_reference(n, lambda xi: f(Phi(a, b, xi)))
+
 # ---------------------------------------------------------------- #
 
 def test_1(p_max, n):
 
     for p in range(p_max+1):
 
-        result_decimal  = gaussian_quadrature(0, 1, n, lambda x: x**p)
+        result_decimal  = quad_gauss(0, 1, n, lambda x: x**p)
         result_fraction = str(Fraction(result_decimal).limit_denominator(1000))
 
         print(result_decimal)
@@ -41,14 +49,14 @@ def test_1(p_max, n):
 
 # ---------------------------------------------------------------- #
 
-def test_polynomial(p_array, n_max):
+def test_2(p_array, n_max):
 
-    a = -1
+    a = 0
     b = 1
 
     for p in p_array:
 
-        approximates = [gaussian_quadrature(a, b, n, lambda x: x**p) for n in range(n_max)]
+        approximates = [quad_gauss(a, b, n, lambda x: x**p) for n in range(n_max)]
         exact = b ** (p+1) / (p+1) - a ** (p+1) / (p+1)
         errors = [abs(approximate - exact) for approximate in approximates]
 
@@ -61,26 +69,31 @@ def test_polynomial(p_array, n_max):
 
     plt.show()
 
-# test_polynomial([1, 2, 4, 8, 16], 10)
+# test_2([1, 2, 4, 8, 16], 10)
 
 # ---------------------------------------------------------------- #
 
 
-def test(f, n_max, exact):
-    a = -1
-    b = 1
+def test_3(a, b, n_max, f, exact):
 
-    approximates = [gaussian_quadrature(a, b, n, f) for n in range(1,n_max)]
-    errors = [abs(approximate - exact) for approximate in approximates]
-    print(errors)
-    plt.semilogy(range(1,n_max), errors)
+    approximates = [quad_gauss(a, b, n, f) for n in range(1, n_max)]
+    errors = [abs(exact - approximate) for approximate in approximates]
+
+    plt.plot(range(1, n_max), errors)
+
     plt.xlabel('$n$')
     plt.ylabel('$\epsilon(n)$')
     plt.grid(linestyle = ':')
 
     plt.show()
 
-f = lambda x: np.exp(x)
-exact = np.exp(1) - np.exp(-1)
+a = -1
+b = 10
+n_max = 10
+f = np.exp
+F = f
+exact = F(b) - F(a)
 
-test(f,10,exact)
+test_3(a, b, n_max, f, exact)
+
+# ---------------------------------------------------------------- #
