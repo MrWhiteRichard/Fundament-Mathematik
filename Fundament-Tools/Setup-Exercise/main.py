18a571791d303c25d0dbc15c48f58987173989ea
Richard Weiss <richard.weiss.tuwien@gmail.com>
2020-10-15 13:20:03 +0200

    Fundament-Tools: Setup-Exercise: upgrades & readability

--- Fundament-Tools/Setup-Exercise/main.py
+++ Fundament-Tools/Setup-Exercise/main.py
@@ -1,15 +1,17 @@
 from auxiliary import setup_exercise
 
-path_exercise_folder = str(input('path_exercise_folder ... '))
-lva_name             = str(input('lva_name             ... '))
-exercise_number      = int(input('exercise_number      ... '))
-exercise_amount      = int(input('exercise_amount      ... '))
-exercise_date        = str(input('exercise date        ... '))
+exercise_folder_path    = str(input('exercise_folder_path    ... '))
+lva_name                = str(input('lva_name                ... '))
+exercise_session_number = int(input('exercise_session_number ... '))
+exercise_number_min     = int(input('exercise_number_min     ... '))
+exercise_number_max     = int(input('exercise_number_max     ... '))
+exercise_date           = str(input('exercise date           ... '))
 
 setup_exercise(
-    path_exercise_folder,
+    exercise_folder_path,
     lva_name,
-    exercise_number,
-    exercise_amount,
+    exercise_session_number,
+    exercise_number_min,
+    exercise_number_max,
     exercise_date
 )
---
+++ Fundament-Tools/Setup-Exercise/test.py
@@ -0,0 +1,17 @@
+from auxiliary import setup_exercise
+
+exercise_folder_path    = r'C:\Users\richa\OneDrive\Dokumente\GitHub\Fundament-Mathematik\WS_2020-21\DGA_UE\Übung_2'
+lva_name                = 'Diskrete und Geometrische Algorithmen'
+exercise_session_number = 2
+exercise_number_min     = 7
+exercise_number_max     = 12
+exercise_date           = '19.10.2020'
+
+setup_exercise(
+    exercise_folder_path,
+    lva_name,
+    exercise_session_number,
+    exercise_number_min,
+    exercise_number_max,
+    exercise_date
+)
