cf550149685e99a214881c98431557deee2a71f4
Richard Weiss <richard.weiss.tuwien@gmail.com>
2020-11-03 18:18:11 +0100

    Fundament-LaTeX: images: - ODEs - renamer

--- Fundament-LaTeX/images/renamer.py
+++ Fundament-LaTeX/images/renamer.py
@@ -1,18 +1,14 @@
 import os
 
-substitutions = {
-    'Hetzl': 'DGA',
-    'Kaltenbaeck': 'Ana1&2',
-    'Grill': 'MassWHT1&2'
-}
+substitutions = {}
 
 for x, y in substitutions.items():
 
-    for file_name in os.listdir():
+    for n, file_name in enumerate([
+        file_name for file_name in os.listdir() if file_name.find(x) != -1
+    ]):
 
-        if file_name.find(x) != -1:
-
-            os.rename(
-                file_name,
-                file_name.replace(x, y)
-            )
+        os.rename(
+            file_name,
+            file_name.replace(x, y)
+        )
