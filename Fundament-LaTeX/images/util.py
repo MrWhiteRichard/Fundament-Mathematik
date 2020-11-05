cfb264a256e6e28da99fbcd92f3299c2418e73a7
Richard Weiss <richard.weiss.tuwien@gmail.com>
2020-11-05 14:09:11 +0100

    Fundament-LaTeX: images: - renamed ODEs images properly - util.py

--- Fundament-LaTeX/images/util.py
+++ Fundament-LaTeX/images/util.py
@@ -1,4 +1,5 @@
 import os
+import codecs
 
 # ---------------------------------------------------------------- #
 
@@ -21,6 +22,8 @@ for x, y in substitutions.items():
 
 # ---------------------------------------------------------------- #
 
+"""
+
 image_file_names = [file_name for file_name in os.listdir() if file_name.find('ODEs') != -1]
 
 for image_file_name in image_file_names:
@@ -28,6 +31,21 @@ for image_file_name in image_file_names:
     os.rename(
         image_file_name,
         image_file_name + '.png'
+    )
+
+"""
+
+# ---------------------------------------------------------------- #
+
+with codecs.open('ODEs.txt', 'r', 'utf-8') as file:
+    content = file.read()
+    content = content.split('\r\n')
+
+for n in range(90):
+
+    os.rename(
+        f'ODEs - {n+1}.png',
+        f'ODEs - {content[n]}.png'
     )
 
 # ---------------------------------------------------------------- #
