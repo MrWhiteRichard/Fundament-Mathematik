a9ef98004e158007aa2b1ebf8e95d0a1910650f1
Richard Weiss <richard.weiss.tuwien@gmail.com>
2020-11-14 11:09:10 +0100

    Fundament-LaTeX: images: util.py: Funktionalitäten in Funktionen gepackt

--- Fundament-LaTeX/images/util.py
+++ Fundament-LaTeX/images/util.py
@@ -1,87 +1,37 @@
+# ---------------------------------------------------------------- #
+
 import os
 import codecs
 
 # ---------------------------------------------------------------- #
 
-"""
+def substitute(substitutions):
 
-substitutions = {}
+    for x, y in substitutions.items():
 
-for x, y in substitutions.items():
+        for n, file_name in enumerate([
+            file_name for file_name in os.listdir() if file_name.find(x) != -1
+        ]):
 
-    for n, file_name in enumerate([
-        file_name for file_name in os.listdir() if file_name.find(x) != -1
-    ]):
+            os.rename(
+                file_name,
+                file_name.replace(x, y)
+            )
+
+# ---------------------------------------------------------------- #
+
+def rename(LVA)
+
+    os.chdir(LVA)
+
+    with codecs.open(LVA + '.txt', 'r', 'utf-8') as file:
+        content = file.read().split('\r\n')
+
+    for n, title in enumerate(content):
 
         os.rename(
-            file_name,
-            file_name.replace(x, y)
+            f'{n}.png',
+            f'{LVA} - {title}.png'
         )
-
-"""
-
-# ---------------------------------------------------------------- #
-
-"""
-
-image_file_names = [file_name for file_name in os.listdir() if file_name.find('ODEs') != -1]
-
-for image_file_name in image_file_names:
-
-    os.rename(
-        image_file_name,
-        image_file_name + '.png'
-    )
-
-"""
-
-# ---------------------------------------------------------------- #
-
-"""
-
-with codecs.open('ODEs.txt', 'r', 'utf-8') as file:
-    content = file.read()
-    content = content.split('\r\n')
-
-for n in range(90):
-
-    os.rename(
-        f'ODEs - {n+1}.png',
-        f'ODEs - {content[n]}.png'
-    )
-
-"""
-
-# ---------------------------------------------------------------- #
-
-"""
-
-with codecs.open('LGM.txt', 'r', 'utf-8') as file:
-    content = file.read()
-    content = content.split('\r\n')
-
-for n in range(225+1):
-
-    os.rename(
-        f'{n}.png',
-        f'LGM - {content[n]}.png'
-    )
-
-"""
-
-# ---------------------------------------------------------------- #
-
-os.chdir('NumPDEs')
-
-with codecs.open('NumPDEs.txt', 'r', 'utf-8') as file:
-    content = file.read()
-    content = content.split('\r\n')
-
-for n, title in enumerate(content):
-
-    os.rename(
-        f'{n}.png',
-        f'NumPDEs - {title}.png'
-    )
 
 # ---------------------------------------------------------------- #
