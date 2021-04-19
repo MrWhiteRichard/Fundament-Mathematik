ddbea188426224be543fded1b8b0f8fe18e86a18
kleon <e11714951@mail.student.tuwien.ac.at>
2021-04-19 15:10:20 +0200

    statistics hw4 and some number theory

---
+++ SS_2021/EStat_UE/Übung_4/Fabian/zehetgruber_fabian_11714951_hw4/estat_hw4_problem4_zehetgruber.R
@@ -0,0 +1,23 @@
+?dnorm
+
+mu <- 5
+sigma <- 2
+
+x <- seq(0, 10, 1/100)
+
+density_function <- dnorm(x, mu, sigma)
+plot(x, density_function, type = "l")
+
+distribution_function <- pnorm(x, mu, sigma)
+plot(x, distribution_function, type = "l")
+
+
+
+n = 50
+random_sample <- rnorm(n, mu, sigma)
+hist(random_sample)
+
+n = 500
+random_sample <- rnorm(n, mu, sigma)
+hist(random_sample)
+
