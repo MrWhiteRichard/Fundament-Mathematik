5a76e7e2a928a89f7132dc098fd254137c49d08f
Richard Weiss <43222374+MrWhiteRichard@users.noreply.github.com>
2021-04-28 19:10:33 +0200

    Fundment-Tools: Setup-Exercise: language support update

--- Fundament-Tools/Setup-Exercise/main.py
+++ Fundament-Tools/Setup-Exercise/main.py
@@ -1,12 +1,13 @@
 from auxiliary import setup_exercise
 
-exercise_folder_path    = str(input('exercise_folder_path    ... '))
-lva_name                = str(input('lva_name                ... '))
-exercise_session_number = int(input('exercise_session_number ... '))
-author_names            = str(input('author_names            ... '))
-exercise_number_min     = int(input('exercise_number_min     ... '))
-exercise_number_max     = int(input('exercise_number_max     ... '))
-exercise_date           = str(input('exercise date           ... '))
+exercise_folder_path    = str(input('exercise_folder_path           ... '))
+lva_name                = str(input('lva_name                       ... '))
+exercise_session_number = int(input('exercise_session_number        ... '))
+author_names            = str(input('author_names ("author 1, ...") ... '))
+exercise_number_min     = int(input('exercise_number_min            ... '))
+exercise_number_max     = int(input('exercise_number_max            ... '))
+exercise_date           = str(input('exercise_date                  ... '))
+language                = str(input('language ("de" / "en")         ... '))
 
 setup_exercise(
     exercise_folder_path,
@@ -15,5 +16,6 @@ setup_exercise(
     author_names,
     exercise_number_min,
     exercise_number_max,
-    exercise_date
+    exercise_date,
+    language
 )
