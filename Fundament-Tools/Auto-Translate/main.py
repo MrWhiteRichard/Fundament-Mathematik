f9e93f4871c1fb894203ba3c8d0296b9ea22a76d
Richard Weiss <43222374+MrWhiteRichard@users.noreply.github.com>
2021-05-05 09:45:22 +0200

    Auto-Translate: Update

--- Fundament-Tools/Auto-Translate/main.py
+++ Fundament-Tools/Auto-Translate/main.py
@@ -7,18 +7,15 @@ from dictionary import dictionary
 
 # ---------------------------------------------------------------- #
 
-while os.path.basename(os.getcwd()) != 'Fundament-Mathematik':
-    os.chdir('..')
+def translate(file_name_old, file_name_new):
 
-os.chdir('Fundament-LaTeX')
+    if os.path.splitext(file_name_old)[1] == '':
+        file_name_old += '.tex'
 
-# ---------------------------------------------------------------- #
+    if os.path.splitext(file_name_new)[1] == '':
+        file_name_new += '.tex'
 
-def translate(file_name):
-
-    assert file_name in ['macros', 'packages', 'environments']
-
-    with codecs.open(file_name + '_de' + '.tex', 'r', 'utf-8') as file:
+    with codecs.open(file_name_old, 'r', 'utf-8') as file:
         file_content_de = file.readlines()
 
     file_content_en = file_content_de
@@ -34,12 +31,31 @@ def translate(file_name):
                         expression_en
                     )
 
-    with codecs.open(file_name + '_en' + '.tex', 'w', 'utf-8') as file:
+    with codecs.open(file_name_new, 'w', 'utf-8') as file:
         file.writelines(file_content_en)
 
 # ---------------------------------------------------------------- #
 
-file_name = input('What file would you like to translate from german to english (macros / packages / environments)? ... ')
-translate(file_name)
+print('Moving to folder "Fundament-LaTeX" ...')
+go_somewhere_else = input('Would you like to go somewhere else ("yes" / "no")? ...' + ' ')
+
+if go_somewhere_else == 'yes':
+
+    path = input('Please input the desired destination path ...' + ' ')
+    os.chdir(path)
+
+else:
+
+    while os.path.basename(os.getcwd()) != 'Fundament-Mathematik':
+        os.chdir('..')
+
+    os.chdir('Fundament-LaTeX')
+
+print('What file would you like to translate from german to english?')
+
+file_name_old = input('Old file name:' + ' ')
+file_name_new = input('New file name:' + ' ')
+
+translate(file_name_old, file_name_new)
 
 # ---------------------------------------------------------------- #
